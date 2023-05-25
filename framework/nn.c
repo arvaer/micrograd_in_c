49168e02cfb6c984fc923c723b150439c4bf81f7
mikey almeida <mikeyalmeida@mikeys-MBP-2.lan>
2023-05-25 00:19:41 -0400

    all necessary operations have been implemented for basic nn framework

--- framework/nn.c
+++ framework/nn.c
@@ -1,8 +1,27 @@
 #define NN_IMPLEMENTATION
 #include "nn.h"
+#include <time.h>
 
 int main(void){
-	Mat m = mat_alloc(2,2);
-	mat_print(m);
+	srand(time(0));
+	float id_data[4] = {1, 0, 0, 1};
+	Mat id = { .rows = 2, .cols = 2, .es = id_data };
+
+
+	Mat a = mat_alloc(1,2);
+	Mat b = mat_alloc(2, 2);
+	Mat M = mat_alloc(1, 2);
+	mat_rand(a, 5, 10);
+	mat_fill(b,1);
+	
+	mat_print(a);
+	printf("************ \n");
+	mat_print(b);
+	printf("************\n");
+	mat_dot(M, a, id);
+	mat_print(M);
+	
+
+
 	return 0;
 }
--- framework/nn.h
+++ framework/nn.h
@@ -15,6 +15,8 @@
 #endif //NN_ASSERT
 
 #define MAT_AT(m,i,j) m.es[(i)*(m).cols + (j)] 
+
+float rand_float(void);
 typedef struct {
 	size_t rows; //float 64
 	size_t cols; //float 64
@@ -22,13 +24,16 @@ typedef struct {
 } Mat;
 
 Mat mat_alloc(size_t rows, size_t cols);
-void mat_mul(Mat dst, const Mat a, const Mat b);
-void mat_sum(Mat dst, const Mat a, const Mat b);
+void mat_rand(Mat m, float low, float high);
+void mat_dot(Mat dst, const Mat a, const Mat b);
+void mat_sum(Mat dst, const Mat a);
+void mat_fill(Mat m, float val);
 
-
-
-
-//mat alloc
+#endif // NN_H_
+#ifdef NN_IMPLEMENTATION
+float rand_float(void){
+	return (float) rand() / (float) RAND_MAX;
+}
 Mat mat_alloc(size_t rows, size_t cols){
 	Mat m;
 	m.rows = rows;
@@ -37,34 +42,63 @@ Mat mat_alloc(size_t rows, size_t cols){
 	NN_ASSERT(m.es != NULL);
 	return m;
 }
+
+void mat_rand(Mat m, float low, float high){
+	for (size_t i = 0; i < m.rows ; i++){
+		for(size_t j = 0; j < m.cols; j++){
+			MAT_AT(m,i,j) = rand_float() * (high - low) + low;
+		}}
+}
+
 //not allocating memory in the function, instead prealloc memory for all matrixes. Multiple into mat c
 //mem cpy signature
-void mat_mul(Mat dst, const Mat a, const Mat b){
-	(void) dst;
-	(void) a;
-	(void) b;
+void mat_dot(Mat dst, const Mat a, const Mat b){
+	//not defined unless the inner sizes are equal
+	//i.e a.cols ==  b.rows
+	NN_ASSERT(a.cols == b.rows);
+	//make sure that dst size is a.cols, b.rows
+	NN_ASSERT(dst.rows = a.cols);
+	NN_ASSERT(dst.cols = b.rows);
+
+	size_t n = a.cols;
+	for(size_t i = 0; i < dst.rows; i++){
+		for(size_t j = 0; j < dst.cols; j++){
+			MAT_AT(dst, i, j) = 0;
+			for(size_t k = 0; k < n; k++){
+				MAT_AT(dst, i, j) += MAT_AT(a, i, k) * MAT_AT(b, k, j);
+			}
+		}
+	}
 
 }
-void mat_sum(Mat dst, const Mat a, const Mat b){
+void mat_sum(Mat dst, const Mat a){
+	//1 x 2 * 2 * x
+	NN_ASSERT(dst.rows == a.rows);
+	NN_ASSERT(dst.cols == a.cols);
+	for(size_t i = 0; i < dst.rows; i ++){
+		for(size_t j = 0; j < dst.cols; j++){
+			MAT_AT(dst, i, j) += MAT_AT(a, i, j);
+		}
+	}
 	(void) dst;
 	(void) a;
-	(void) b;
 
 }
 void mat_print(const Mat m){
 	for (size_t i = 0; i < m.rows; i++){
 		for(size_t j = 0; j < m.cols; j++){
-			printf("%f ", MAT_AT(m,i,j));
+				printf("%f ", MAT_AT(m,i,j));
 			}
 		printf("\n");
 	}
 }
-	
 
-
-
-#endif // NN_H_
-
-#ifdef NN_IMPLEMENTATION
+void mat_fill(Mat m, float val){
+	for (size_t i = 0; i < m.rows; i++){
+		for(size_t j = 0; j < m.cols; j++){
+			MAT_AT(m, i, j) = val;	
+		}
+	}
+}
 
 #endif // NN_IMPLEMENTATION
